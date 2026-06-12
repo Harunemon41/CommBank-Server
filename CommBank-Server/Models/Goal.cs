27fee195a282228edf07cbc78d4a2cd717f3e686
Harunemon41 <yesiamxyzemon3904@gmail.com>
2026-06-12 21:04:44 +0100

    Refactor Goal class properties and add Icon field
    Rearranged properties and added optional Icon field.

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -1,30 +1,32 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace CommBank.Models;
-
-public class Goal
+namespace CommBank.Models
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public class Goal
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
-    public string? Name { get; set; }
+        public string? Name { get; set; }
 
-    public UInt64 TargetAmount { get; set; } = 0;
+        public DateTime TargetDate { get; set; }
 
-    public DateTime TargetDate { get; set; }
+        public double TargetAmount { get; set; }
 
-    public double Balance { get; set; } = 0.00;
+        public double Balance { get; set; }
 
-    public DateTime Created { get; set; } = DateTime.Now;
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string>? TransactionIds { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public List<string>? TransactionIds { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string>? TagIds { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public List<string>? TagIds { get; set; }
+        // Optional Icon field added for Task 1
+        public string? Icon { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? UserId { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? UserId { get; set; }
+    }
 }
