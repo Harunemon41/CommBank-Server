50a360871d6559f39c38eeb9d5217f6283b908da
Harunemon41 <yesiamxyzemon3904@gmail.com>
2026-06-12 21:08:50 +0100

    Refactor GoalsService to use Options for settings

--- CommBank-Server/Services/GoalService.cs
+++ CommBank-Server/Services/GoalService.cs
@@ -1,32 +1,38 @@
 ﻿using CommBank.Models;
+using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
-namespace CommBank.Services;
-
-public class GoalsService : IGoalsService
+namespace CommBank.Services
 {
-    private readonly IMongoCollection<Goal> _goalsCollection;
-
-    public GoalsService(IMongoDatabase mongoDatabase)
+    public class GoalsService
     {
-        _goalsCollection = mongoDatabase.GetCollection<Goal>("Goals");
+        private readonly IMongoCollection<Goal> _goalsCollection;
+
+        public GoalsService(IOptions<CommBankDatabaseSettings> commBankDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                commBankDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                commBankDatabaseSettings.Value.DatabaseName);
+
+            _goalsCollection = mongoDatabase.GetCollection<Goal>(
+                commBankDatabaseSettings.Value.GoalsCollectionName);
+        }
+
+        public async Task<List<Goal>> GetAsync() =>
+            await _goalsCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Goal?> GetAsync(string id) =>
+            await _goalsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Goal newGoal) =>
+            await _goalsCollection.InsertOneAsync(newGoal);
+
+        public async Task UpdateAsync(string id, Goal updatedGoal) =>
+            await _goalsCollection.ReplaceOneAsync(x => x.Id == id, updatedGoal);
+
+        public async Task RemoveAsync(string id) =>
+            await _goalsCollection.DeleteOneAsync(x => x.Id == id);
     }
-
-    public async Task<List<Goal>> GetAsync() =>
-        await _goalsCollection.Find(_ => true).ToListAsync();
-
-    public async Task<List<Goal>?> GetForUserAsync(string id) =>
-        await _goalsCollection.Find(x => x.UserId == id).ToListAsync();
-
-    public async Task<Goal?> GetAsync(string id) =>
-        await _goalsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
-
-    public async Task CreateAsync(Goal newGoal) =>
-        await _goalsCollection.InsertOneAsync(newGoal);
-
-    public async Task UpdateAsync(string id, Goal updatedGoal) =>
-        await _goalsCollection.ReplaceOneAsync(x => x.Id == id, updatedGoal);
-
-    public async Task RemoveAsync(string id) =>
-        await _goalsCollection.DeleteOneAsync(x => x.Id == id);
 }
